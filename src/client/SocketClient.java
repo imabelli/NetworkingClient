6a608fe21e68632903c29798e10c5fca5922e4b7
imabelli <isabeljadeli@yahoo.com>
2019-01-19 12:58:29 -0600

    changed name to SocketClient

--- src/client/SocketClient.java
+++ src/client/SocketClient.java
@@ -3,16 +3,19 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
-public class LearningSocket {
+public class SocketClient {
     public static void main(String[] args) {
         String hostname = "127.0.0.1";
-        int port = 8000;
+        int port = 900;
 
+        //String hostname = "google.com";
+        //int port = 80;
+        //server input output stream are binary
         try{
             Socket socket = new Socket(hostname, port);
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-            writer.println("GET /abc");
+            OutputStream output = socket.getOutputStream(); //what you send
+            PrintWriter writer = new PrintWriter(output, true); //converts text to
+            writer.println("GET /");
             writer.println();
             writer.flush();
 
